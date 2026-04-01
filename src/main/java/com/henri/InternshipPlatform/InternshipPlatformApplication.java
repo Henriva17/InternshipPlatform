cc752bcc84635888b04824326d3897d8a912e1ce
Henriva17 <henritresor405@gmail.com>
2026-04-01 12:37:20 +0200

    Establish a Database SQL connecton with Spring Boot

---
+++ src/main/java/com/henri/InternshipPlatform/InternshipPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.henri.InternshipPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henri/InternshipPlatform/InternshipPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henri.InternshipPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
